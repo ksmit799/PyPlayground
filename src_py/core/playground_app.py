e5f05dd24e94ce09adad6d1e76d819b31a6d59c3
Kyle Smith <kyle.smithy2@gmail.com>
2019-12-01 14:07:35 +1000

    playground_app: Added PlaygroundApp class
    * Simplifies application creation through Python.

---
+++ src_py/core/playground_app.py
@@ -0,0 +1,47 @@
+"""
+"""
+
+__all__ = ['PlaygroundApp']
+
+import builtins
+
+from pyplayground.core.playground_base import PlaygroundBase
+from pyplayground.core.playground_exception import PlaygroundException
+
+class PlaygroundApp:
+
+    def __init__(self):
+        # Is the main app loop running?
+        # Mangle to avoid potential conflicts in inheritors.
+        self.__running = False
+
+        self.instance = PlaygroundBase.instance()
+
+        self.window = self.instance.window
+        self.renderer = self.instance.renderer
+
+        # Make sure we're not instantiating more then one PlaygroundApp.
+        if hasattr(builtins, 'playground_app'):
+            raise PlaygroundException("Attempted to create multiple PlaygroundApp instances!")
+
+        # TODO(Ksmit799): Currently, we make playground_app a builtin singleton pointer.
+        # This is mainly for ease of use (especially for new programmers), however in the
+        # future we may want to make this a global variable.
+        builtins.playground_app = self
+
+    def loop(self):
+        """Run once through the PyPlayground app loop."""
+        self.renderer.render()
+
+    def run(self):
+        """Start running the blocking app loop."""
+        self.__running = True
+
+        while self.__running:
+            self.loop()
+
+    def quit(self):
+        """Stop running the app loop and cleanup."""
+        self.__running = False
+        del builtins.playground_app
+        del self.instance
