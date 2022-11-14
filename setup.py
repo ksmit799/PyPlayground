f5d9e9bc631db5a93758543db3fb2aa73966a0ec
Kylie Smith <kyle.smithy2@gmail.com>
2022-11-14 23:38:31 +1000

    Updated build system

--- setup.py
+++ setup.py
@@ -1,73 +1,143 @@
 import os
 import re
-import sys
-import platform
 import subprocess
+import sys
+from pathlib import Path
 
-from setuptools import setup, Extension
+from setuptools import Extension, setup
 from setuptools.command.build_ext import build_ext
-from distutils.version import LooseVersion
 
+# Convert distutils Windows platform specifiers to CMake -A arguments
+PLAT_TO_CMAKE = {
+    "win32": "Win32",
+    "win-amd64": "x64",
+    "win-arm32": "ARM",
+    "win-arm64": "ARM64",
+}
+
+
+# A CMakeExtension needs a sourcedir instead of a file list.
+# The name must be the _single_ output extension from the CMake build.
+# If you need multiple extensions, see scikit-build.
 class CMakeExtension(Extension):
+    def __init__(self, name: str, sourcedir: str = "") -> None:
+        super().__init__(name, sources=[])
+        self.sourcedir = os.fspath(Path(sourcedir).resolve())
 
-	def __init__(self, name, sourcedir=''):
-		Extension.__init__(self, name, sources=[])
-		self.sourcedir = os.path.abspath(sourcedir)
 
 class CMakeBuild(build_ext):
-	
-	def run(self):
-		try:
-			out = subprocess.check_output(['cmake', '--version'])
-		except OSError:
-			raise RuntimeError("CMake must be installed to build the following extensions: " +
-							   ", ".join(e.name for e in self.extensions))
+    def build_extension(self, ext: CMakeExtension) -> None:
+        # Must be in this form due to bug in .resolve() only fixed in Python 3.10+
+        ext_fullpath = Path.cwd() / self.get_ext_fullpath(ext.name)  # type: ignore[no-untyped-call]
+        extdir = ext_fullpath.parent.resolve()
 
-		if platform.system() == "Windows":
-			cmake_version = LooseVersion(re.search(r'version\s*([\d.]+)', out.decode()).group(1))
-			if cmake_version < '3.2.0':
-				raise RuntimeError("CMake >= 3.2.0 is required on Windows")
+        # Using this requires trailing slash for auto-detection & inclusion of
+        # auxiliary "native" libs
 
-		# Build extensions.
-		for ext in self.extensions:
-			self.build_extension(ext)
+        debug = int(os.environ.get("DEBUG", 0)) if self.debug is None else self.debug
+        cfg = "Debug" if debug else "Release"
 
-	def build_extension(self, ext):
-		extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
-		cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
-					  '-DPYTHON_EXECUTABLE=' + sys.executable]
+        # CMake lets you override the generator - we need to check this.
+        # Can be set with Conda-Build, for example.
+        cmake_generator = os.environ.get("CMAKE_GENERATOR", "")
 
-		cfg = 'Debug' if self.debug else 'Release'
-		build_args = ['--config', cfg]
+        # Set Python_EXECUTABLE instead if you use PYBIND11_FINDPYTHON
+        # EXAMPLE_VERSION_INFO shows you how to pass a value into the C++ code
+        # from Python.
+        cmake_args = [
+            f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={extdir}{os.sep}",
+            f"-DPYTHON_EXECUTABLE={sys.executable}",
+            f"-DCMAKE_BUILD_TYPE={cfg}",  # not used on MSVC, but no harm
+        ]
+        build_args = []
+        # Adding CMake arguments set as environment variable
+        # (needed e.g. to build for ARM OSx on conda-forge)
+        if "CMAKE_ARGS" in os.environ:
+            cmake_args += [item for item in os.environ["CMAKE_ARGS"].split(" ") if item]
 
-		if platform.system() == "Windows":
-			cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir)]
-			if sys.maxsize > 2**32:
-				cmake_args += ['-A', 'x64']
-			build_args += ['--', '/m']
-		else:
-			cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
-			build_args += ['--', '-j2']
+        # In this example, we pass in the version to C++. You might not need to.
+        cmake_args += [f"-DEXAMPLE_VERSION_INFO={self.distribution.get_version()}"]  # type: ignore[attr-defined]
 
-		env = os.environ.copy()
-		env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''),
-															  self.distribution.get_version())
-		if not os.path.exists(self.build_temp):
-			os.makedirs(self.build_temp)
-		subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
-		subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=self.build_temp)
+        if self.compiler.compiler_type != "msvc":
+            # Using Ninja-build since it a) is available as a wheel and b)
+            # multithreads automatically. MSVC would require all variables be
+            # exported for Ninja to pick it up, which is a little tricky to do.
+            # Users can override the generator with CMAKE_GENERATOR in CMake
+            # 3.15+.
+            if not cmake_generator or cmake_generator == "Ninja":
+                try:
+                    import ninja  # noqa: F401
 
+                    ninja_executable_path = Path(ninja.BIN_DIR) / "ninja"
+                    cmake_args += [
+                        "-GNinja",
+                        f"-DCMAKE_MAKE_PROGRAM:FILEPATH={ninja_executable_path}",
+                    ]
+                except ImportError:
+                    pass
+
+        else:
+
+            # Single config generators are handled "normally"
+            single_config = any(x in cmake_generator for x in {"NMake", "Ninja"})
+
+            # CMake allows an arch-in-generator style for backward compatibility
+            contains_arch = any(x in cmake_generator for x in {"ARM", "Win64"})
+
+            # Specify the arch if using MSVC generator, but only if it doesn't
+            # contain a backward-compatibility arch spec already in the
+            # generator name.
+            if not single_config and not contains_arch:
+                cmake_args += ["-A", PLAT_TO_CMAKE[self.plat_name]]
+
+            # Multi-config generators have a different way to specify configs
+            if not single_config:
+                cmake_args += [
+                    f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{cfg.upper()}={extdir}"
+                ]
+                build_args += ["--config", cfg]
+
+        if sys.platform.startswith("darwin"):
+            # Cross-compile support for macOS - respect ARCHFLAGS if set
+            archs = re.findall(r"-arch (\S+)", os.environ.get("ARCHFLAGS", ""))
+            if archs:
+                cmake_args += ["-DCMAKE_OSX_ARCHITECTURES={}".format(";".join(archs))]
+
+        # Set CMAKE_BUILD_PARALLEL_LEVEL to control the parallel build level
+        # across all generators.
+        if "CMAKE_BUILD_PARALLEL_LEVEL" not in os.environ:
+            # self.parallel is a Python 3 only way to set parallel jobs by hand
+            # using -j in the build_ext call, not supported by pip or PyPA-build.
+            if hasattr(self, "parallel") and self.parallel:
+                # CMake 3.12+ only.
+                build_args += [f"-j{self.parallel}"]
+
+        build_temp = Path(self.build_temp) / ext.name
+        if not build_temp.exists():
+            build_temp.mkdir(parents=True)
+
+        subprocess.run(
+            ["cmake", ext.sourcedir] + cmake_args, cwd=build_temp, check=True
+        )
+        subprocess.run(
+            ["cmake", "--build", "."] + build_args, cwd=build_temp, check=True
+        )
+
+
+# The information here can also be placed in setup.cfg - better separation of
+# logic and declaration, and simpler if you include description/version in a file.
 setup(
-	name='PyPlayground',
-	version='1.0.0',
-	author='Kyle Smith',
-	author_email='kyle.smith@trapdoorstudios.com.au',
-	description='The PyPlayground open-source game engine',
-	long_description='',
-	packages=['pyplayground', 'pyplayground.core',
-			  'pyplayground.render', 'pyplayground.task'],
-	package_dir={'pyplayground': 'src_py'},
-	ext_modules=[CMakeExtension('PyPlayground')],
-	cmdclass=dict(build_ext=CMakeBuild),
-	zip_safe=False,
+    name="PyPlayground",
+    version="1.0.0",
+    author="Kylie Smith",
+    author_email="kylie.smithy01@gmail.com",
+    description="The PyPlayground open-source game engine",
+    long_description="",
+    packages=["pyplayground", "pyplayground.core", "pyplayground.render", "pyplayground.task"],
+    package_dir={"pyplayground": "src_py"},
+    ext_modules=[CMakeExtension("PyPlayground")],
+    cmdclass={"build_ext": CMakeBuild},
+    zip_safe=False,
+    extras_require={"test": ["pytest>=7.2.0", "black>=22.10.0"]},
+    python_requires=">=3.10",
 )
